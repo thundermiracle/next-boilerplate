a9b22ff2b7aaf623c4f9fbb63ad0f6900507881e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 12:27:46 +0000

    Deploy thundermiracle/next-boilerplate to github.com/thundermiracle/next-boilerplate.git:gh-pages

---
+++ _next/static/w2c4zZxHC6uHqFw0m1UYw/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["d0a3"],{RNiq:function(n,e,a){"use strict";a.r(e);var t=a("mXGw"),u=a.n(t);e.default=function(){return u.a.createElement("div",null,"Please select a menu.")}},TqC3:function(n,e,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){var n=a("RNiq");return{page:n.default||n}}])}},[["TqC3","5d41","9da1"]]]);
